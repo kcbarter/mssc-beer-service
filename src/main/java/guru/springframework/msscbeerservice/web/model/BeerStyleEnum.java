d4a4fa89943923acda675aa663d526af2bdb89ff
kcbar <Kbarter@mail.greenriver.edu>
2020-04-09 16:31:48 -0700

    Created Data model for beer.

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LOGGER,
+    PISLSNER,
+    ALE,
+    STOUT,
+    GOSE,
+    IPA,
+    WHEAT,
+    SAISON,
+    PORTER
+}
